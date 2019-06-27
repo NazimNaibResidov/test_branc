93bf894f482295b82008435fda26ef0c7577b727
Naib Residov <residovnaib@gmail.com>
2019-06-27 19:17:19 +0300

    for testing

---
+++ Model/Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test.BrnchTest.Model
+{
+   public class Data
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
